72db47c085afce8ba19889fd2c5d0c5c6389fabf
Niemand <moritzmechelk@gmail.com>
2019-11-29 17:55:40 +0100

    transition to zeit

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const adminRoleID = '649708528310681640';
 var client = new discord.Client();
 
 var cmdmap = {
-	say: cmdSay,
 	shader: cmdShader
 };
 
@@ -22,28 +21,33 @@ function updateShaderData(data, successFunct) {
 	});
 }
 
-function cmdSay(msg, arguments) {
-	msg.channel.send(arguments.join(' '));
-}
-
 function cmdShader(msg, arguments, author) {
 	// if (!author.guild.roles.find((r) => r.id == adminRoleID)) return;
 
 	if (arguments[0] === 'init') {
-		var channelDev = msg.mentions.users.first();
-		if (channelDev) {
-			shaderChannelData[msg.channel] = { shaderDevID: channelDev.id };
+		var channelDevs = msg.mentions.users.keyArray();
+		if (msg.mentions.users.first()) {
+			shaderChannelData[msg.channel] = { devsID: channelDevs };
 			updateShaderData(shaderChannelData, () => {
-				embeds.feedback(msg.channel, `Successfully linked <@${shaderChannelData[msg.channel].shaderDevID}> with ${msg.channel}.`);
+				embeds.feedback(msg.channel, `Successfully linked <@${shaderChannelData[msg.channel].devsID.join('>, <@')}> with ${msg.channel}.`);
 			});
 		} else {
-			embeds.errorSyntax(msg.channel, '!shader init @ShaderDeveloper');
+			embeds.errorSyntax(msg.channel, '!shader init @shader_developer [...]');
+		}
+	} else if (arguments[0] === 'reset') {
+		if (shaderChannelData[msg.channel]) {
+			delete shaderChannelData[msg.channel];
+			updateShaderData(shaderChannelData);
+			embeds.feedback(msg.channel, `Successfully reset ${msg.channel}.`);
+		} else {
+			embeds.error(msg.channel, `${msg.channel} is not initialized as a shader channel.`);
 		}
 	} else if (arguments[0] === 'info') {
 		var channelData = shaderChannelData[msg.channel];
-		if (channelData) embeds.feedback(msg.channel, `Channel: ${msg.channel}\nDeveloper: <@${channelData.shaderDevID}>`);
+		if (channelData) embeds.feedback(msg.channel, `Channel: ${msg.channel}\nDeveloper: <@${channelData.devsID.join('>, <@')}>`);
+		else embeds.error(msg.channel, `${msg.channel} is not initialized as a shader channel.`);
 	} else {
-		embeds.errorSyntax(msg.channel, '!shader <info|init>');
+		embeds.errorSyntax(msg.channel, '!shader <info|init|reset>');
 	}
 }
 
@@ -58,8 +62,8 @@ client.on('ready', () => {
 client.on('message', (msg) => {
 	var content = msg.content;
 	var author = msg.member;
-	var channel = msg.channel;
-	var guild = msg.guild;
+	// var channel = msg.channel;
+	// var guild = msg.guild;
 
 	if (author.id !== client.user.id && content.startsWith(config.prefix)) {
 		var invoke = content.split(' ')[0].substr(config.prefix.length);
@@ -79,11 +83,14 @@ client.on('guildMemberAdd', (member) => {
 });
 
 client.on('channelDelete', (channel) => {
-	delete shaderChannelData[channel];
-	updateShaderData(shaderChannelData);
+	if (shaderChannelData[channel]) {
+		delete shaderChannelData[channel];
+		updateShaderData(shaderChannelData);
+	}
 });
 
-client.login(process.env.TOKEN);
+require('http').createServer().listen(3000);
+client.login(process.env.token);
 
 // const token = JSON.parse(fs.readFileSync('token.json', 'utf8'));
 // client.login(token.token);
