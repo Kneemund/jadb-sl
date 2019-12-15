fc67cfeee0315a9dc72f01f62d1a3619f5d57d15
Niemand <moritzmechelk@gmail.com>
2019-12-15 21:37:27 +0100

    added response to "rip"

--- events/message.js
+++ events/message.js
@@ -1,8 +1,14 @@
 const embeds = require('../util/embeds.js');
 
+function includeReply(message) {
+	if (message.content.toLowerCase().includes('no u')) message.channel.send('no u');
+	if (message.content.toLowerCase().includes('rip'))
+		message.channel.send('https://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2013/11/rip-786x305.jpg');
+}
+
 module.exports = (client, message) => {
 	if (message.author.bot) return;
-	if (message.content.toLowerCase().includes('no u')) message.channel.send('no u');
+	includeReply(message);
 	if (!message.content.startsWith(client.config.prefix)) return;
 
 	const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
